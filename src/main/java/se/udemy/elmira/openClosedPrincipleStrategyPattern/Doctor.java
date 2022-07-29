63e40e77a8915668bdba3b43af92a91b913820d8
EELMMAD <elmiramadadi@gmail.com>
2022-07-29 22:33:38 +0200

    open closed principle

---
+++ src/main/java/se/udemy/elmira/openClosedPrincipleStrategyPattern/Doctor.java
@@ -0,0 +1,23 @@
+package se.udemy.elmira.openClosedPrincipleStrategyPattern;
+
+public class Doctor extends Employee {
+
+    public Doctor(int id, String name, String department, boolean working) {
+        super(id, name, department, working);
+        System.out.println("Doctor in action...");
+    }
+
+    private void prescribeMedicine() {
+        System.out.println("Prescribe Medicine");
+    }
+
+    private void diagnosePatients() {
+        System.out.println("Diagnose Patient");
+    }
+
+    @Override
+    public void performDuties() {
+        prescribeMedicine();
+        diagnosePatients();
+    }
+}
---
+++ src/main/java/se/udemy/elmira/openClosedPrincipleStrategyPattern/HospitalManagement.java
@@ -0,0 +1,8 @@
+package se.udemy.elmira.openClosedPrincipleStrategyPattern;
+
+public class HospitalManagement {
+
+    public void callUpon(Employee employee){
+       employee.performDuties();
+    }
+}
