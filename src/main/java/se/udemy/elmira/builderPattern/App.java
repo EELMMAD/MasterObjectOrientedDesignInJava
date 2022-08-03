de989c3e46a7a8786fe7f11b5539faa29fbc9dc3
EELMMAD <elmiramadadi@gmail.com>
2022-08-03 23:16:02 +0200

    Add App.java

---
+++ src/main/java/se/udemy/elmira/builderPattern/App.java
@@ -0,0 +1,10 @@
+package se.udemy.elmira.builderPattern;
+
+public class App {
+    public static void main(String[] args) {
+        User websiteUser = new User.Builder("bobMax", "bobMax@gmail.com")
+                .firstName("bob").lastName("max").build();
+
+        System.out.println(websiteUser);
+    }
+}
