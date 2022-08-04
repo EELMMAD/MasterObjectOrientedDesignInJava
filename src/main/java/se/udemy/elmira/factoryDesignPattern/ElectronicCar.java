fe54d89866fefb9121e6bce981278119d1ee5c21
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 08:37:38 +0200

    Rename the class

--- src/main/java/se/udemy/elmira/factoryDesignPattern/ElectronicCar.java
+++ src/main/java/se/udemy/elmira/factoryDesignPattern/ElectronicCar.java
@@ -1,6 +1,6 @@
 package se.udemy.elmira.factoryDesignPattern;
 
-public class ElectricCar implements Vehicle{
+public class ElectronicCar implements Vehicle{
     @Override
     public void startEngine() {
         System.out.println("pushed button started electronic car's engine...");
