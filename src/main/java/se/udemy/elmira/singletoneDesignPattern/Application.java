6aa97cc37b175ae283f22f755ba7720b15fc2ff8
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 11:42:32 +0200

    Add Application.java

---
+++ src/main/java/se/udemy/elmira/singletoneDesignPattern/Application.java
@@ -0,0 +1,14 @@
+package se.udemy.elmira.singletoneDesignPattern;
+
+public class Application {
+    public static void main(String[] args) {
+        PerformanceStage stage = PerformanceStage.getInstance();
+
+        PerformanceStage stage2 = new PerformanceStage();
+        PerformanceStage stage3 = new PerformanceStage();
+        PerformanceStage stage4 = new PerformanceStage();
+        PerformanceStage stage5 = new PerformanceStage();
+
+        System.out.println(stage3.getCounter());
+    }
+}
