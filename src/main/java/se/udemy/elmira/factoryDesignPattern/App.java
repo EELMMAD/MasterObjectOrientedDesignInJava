c1c714f0712408384aae42e620ef0af993443656
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 08:45:05 +0200

    add main method to Add.java

--- src/main/java/se/udemy/elmira/factoryDesignPattern/App.java
+++ src/main/java/se/udemy/elmira/factoryDesignPattern/App.java
@@ -1,4 +1,10 @@
 package se.udemy.elmira.factoryDesignPattern;
 
 public class App {
+    public static void main(String[] args) {
+        VehicleFactory vehicleFactory = new VehicleFactory();
+
+        Vehicle vehicle = new VehicleFactory().getVehicle(VehicleType.CAR);
+        vehicle.startEngine();
+    }
 }
