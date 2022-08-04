b6db31a398a9724d1f3329a7308c44615d2fd996
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 08:38:11 +0200

    add VehicleType enum

---
+++ src/main/java/se/udemy/elmira/factoryDesignPattern/VehicleType.java
@@ -0,0 +1,20 @@
+package se.udemy.elmira.factoryDesignPattern;
+
+public enum VehicleType {
+    TRUCK{
+        public Vehicle getVehicle(){
+            return new Truck();
+        }
+    },
+    CAR{
+        public Vehicle getVehicle(){
+            return new Car();
+        }
+    },
+    ELECTRIC{
+        public Vehicle getVehicle(){
+            return new ElectronicCar();
+        }
+    };
+    abstract Vehicle getVehicle();
+}
