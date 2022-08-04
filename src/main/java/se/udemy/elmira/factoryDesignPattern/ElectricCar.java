0b42b43a189b5b4dad4022a650541f95f8cb7598
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 08:25:51 +0200

    implement override method

--- src/main/java/se/udemy/elmira/factoryDesignPattern/ElectricCar.java
+++ src/main/java/se/udemy/elmira/factoryDesignPattern/ElectricCar.java
@@ -1,4 +1,9 @@
 package se.udemy.elmira.factoryDesignPattern;
 
-public class ElectricCar {
-}
+public class ElectricCar implements Vehicle{
+    @Override
+    public void startEngine() {
+        System.out.println("pushed button started electronic car's engine...");
+        }
+    }
+
