8dee297d74747aa1d73c357da39511401685f065
EELMMAD <elmiramadadi@gmail.com>
2022-08-04 08:27:19 +0200

    implement override method

--- src/main/java/se/udemy/elmira/factoryDesignPattern/Truck.java
+++ src/main/java/se/udemy/elmira/factoryDesignPattern/Truck.java
@@ -1,5 +1,8 @@
 package se.udemy.elmira.factoryDesignPattern;
 
-public class Truck {
-
+public class Truck implements Vehicle{
+    @Override
+    public void startEngine() {
+        System.out.println("started a large engine of truck...");
+    }
 }
